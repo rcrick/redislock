5b2489648f300881af47e7917eaaa835c4742ee8
zixuan.xu <zixuan.xu@shopee.com>
2021-06-24 15:37:17 +0800

    Fix code

--- main.go
+++ main.go
@@ -15,19 +15,19 @@ func main() {
 	client := v8.NewClient(&v8.Options{Addr: "127.0.0.1:6379"})
 	wg := sync.WaitGroup{}
 	
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 3; i++ {
 		wg.Add(1)
 		go func(ownerid int) {
-			lock1 := rlock.RLock{
+			lock := rlock.RLock{
 				Key:      "1",
 				OwnerID:  strconv.Itoa(ownerid),
 				Client:   client,
 				Ttl:      20 * time.Second,
 				Deadline: 100 * time.Second,
 			}
-			lock1.Lock()
+			lock.Lock()
 			time.Sleep(time.Second)
-			err := lock1.Unlock()
+			err := lock.Unlock()
 			if err != nil {
 				fmt.Println(err)
 			}
